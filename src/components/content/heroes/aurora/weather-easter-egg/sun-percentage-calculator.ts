fd1b56a5d37791041b8472f184976ee5016abe6a
noasanto <n@bensanto.com>
2023-07-18 18:03:51 +0200

    feat: added get nighttime percentage

--- src/components/content/heroes/aurora/weather-easter-egg/sun-percentage-calculator.ts
+++ src/components/content/heroes/aurora/weather-easter-egg/sun-percentage-calculator.ts
@@ -19,3 +19,25 @@ export const getSunlightPercentage = (
   }
   return Math.round(sunlightPercentage);
 };
+
+export const getNighttimePercentage = (
+  sunriseTime: number,
+  sunsetTime: number,
+) => {
+  const time = +new Date();
+
+  const totalNighttimeMinutes = (sunriseTime - sunsetTime) / (1000 * 60);
+  if (totalNighttimeMinutes <= 0) {
+    return 0;
+  }
+  const currentMinutes = (time - sunsetTime) / (1000 * 60);
+  const nighttimePercentage = (currentMinutes / totalNighttimeMinutes) * 100;
+
+  if (nighttimePercentage < 0) {
+    return 0;
+  }
+  if (nighttimePercentage > 100) {
+    return 100;
+  }
+  return Math.round(nighttimePercentage);
+};
